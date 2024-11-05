4acaabfeb8f5c212855279e8c054bfbbba3d4dbc
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2024-11-05 14:54:09 +0100

    Exercise 1-7

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,65 @@
 let names = ["Alex", "Sam", "John", "Lucia", "Marco", "Kristina"];
 
 // Your code here
+
+
+// exercise 1
+// function exercise1() {
+//   console.log("Hi! My name is Alex");
+// }
+// exercise1();
+
+// exercise 2
+// let namePrint = (name) => {
+//   return `Hi! My name is ${name}`;
+// };
+// console.log(namePrint("Alex"));
+
+// exercise 3
+// let namePrint = (name = "Alex") => {
+//   return `Hi! My name is ${name}`;
+// };
+// console.log(namePrint());
+
+// exercise 4
+// let fullName = (name = "Alex", lastName) => {
+//   return `Hi! My name is ${name} ${lastName}`;
+// };
+// console.log(fullName("May", "Meow"));
+
+// exercise 5
+// let fullName = (name = "Alex", lastName = `${name}sson`) => {
+//   return `Hi! My name is ${name} ${lastName}`;
+// };
+// console.log(fullName("May", "Meow"));
+
+// exercise 6
+
+// let addParagraph = () => {
+//   let exercise6 = document.getElementById("exercise6");
+//   let newParagraph = document.createElement("p");
+//   newParagraph.textContent = "Here I am!";
+//   newParagraph.className = "exercise_answer";
+//   exercise6.appendChild(newParagraph);
+// }
+// addParagraph();
+
+// exercise 7
+// let addParagraph = (text) => {
+//   let exercise7 = document.getElementById("exercise7");
+//   let newParagraph = document.createElement("p");
+//   newParagraph.textContent = text;
+//   newParagraph.className = "exercise_answer";
+//   exercise7.appendChild(newParagraph);
+// }
+// addParagraph("Hey there! 😊");
+
+// exercise 8
+// let addParagraph = (text) => {
+//     let exercise6 = document.getElementById("exercise7");
+//     let newParagraph = document.createElement("p");
+//     newParagraph.textContent = text;
+//     newParagraph.className = "exercise_answer";
+//     exercise6.appendChild(newParagraph);
+//   }
+//   addParagraph("Hey there! 😊");
